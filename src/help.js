e3e2a6bac5dedac2e7030c3071540be6541deaf1
bruxinhok <77681118+bruxinhok@users.noreply.github.com>
2021-02-11 13:38:05 -0300

    Add files via upload

---
+++ src/help.js
@@ -0,0 +1,55 @@
+const help = (prefix) => {
+	return `𝐵𝑂𝑇 𝐶𝑅𝐼𝐴𝐷𝑂 𝑃𝑂𝑅 𝐵𝑌 𝐵𝑅𝑈𝑋𝐼𝑁𝐻𝑂 𝑀𝑂𝐷𝑆 𝑊𝐴𝑅
+≪━─━─━─━─★─━─━─━─━≫
+
+≪━─━─━─━──━─━─━─━≫≪━─━─━─━──━─━
+𝑂𝑈𝑇𝑅𝑂𝑆 𝐶𝑂𝑀𝐴𝑁𝐷𝑂𝑆 𝑆𝐸𝑅𝐴𝑂 𝐴𝐷𝐼𝐶𝐼𝑂𝑁𝐴𝐷𝑂𝑆 𝐸𝑀 𝐵𝑅𝐸𝑉𝐸 𝐷𝐸𝑆𝐷𝐸 𝐽𝐴 𝐴𝐺𝑅𝐴𝐷𝐸Ç𝑂 𝑄𝑈𝐸𝑀 𝑃𝑂𝐷𝐸𝑅 𝐷𝐴𝑅 𝑈𝑀𝐴 𝐹𝑂𝑅Ç𝐴 𝐿𝐴 𝑁𝑂 𝑀𝐸𝑈 𝐶𝐴𝑁𝐴𝐿:https://youtu.be/hwJ31n05Qwk
+━━━━━━━━❪❂❫━━━━━━━━
+
+╭────❍ *MÚSICA*.
+╰─❖ *${prefix}play*
+╭─❖ *${prefix}ytmp3 (LINK)*
+╰─❖ *${prefix}ytmp4 (LINK)*
+
+╭────❍ *RECURSOS MAKER*
+╰─❖ *${prefix}sticker*
+╭─❖ *${prefix}stickernobg*
+╰─❖ *${prefix}toimg*
+╭─❖ *${prefix}tsticker*
+╰─❖
+╭────❍ *OUTROS COMANDOS*
+╰─❖ *${prefix}gato*
+╭─❖ *${prefix}meme*
+╰─❖ *${prefix}memeindo*
+╭─❖ *${prefix}gtts*
+╰─❖ *${prefix}loli*
+╭─❖ *${prefix}simi*
+╰─❖ *${prefix}ocr*
+╭─❖ *${prefix}nsfwloli*
+╰─❖ *${prefix}url2img*
+╭─❖ *${prefix}wait*
+╰─❖ *${prefix}setprefix*
+╭─❖ *${prefix}imagem*
+╰─❖
+╭────❍ *GRUPO*
+╰─❖ *${prefix}add*
+╭─❖ *${prefix}remover*
+╰─❖ *${prefix}promote*
+╭─❖ *${prefix}demote*
+╰─❖ *${prefix}fechar*
+╭─❖ *${prefix}abrir*
+╰─❖ *${prefix}linkgroup*
+╭─❖ *${prefix}leave*
+╰─❖ *${prefix}tagall*
+╭─❖ *${prefix}simih*
+╰─❖ *${prefix}leave*
+╭─❖ *${prefix}persengay*
+╰─❖ *${prefix}artinome*
+╭─❖ *${prefix}listadmins*
+╰─❖ *${prefix}tagall2*
+╭─❖ *${prefix}welcome*
+╰─❖ *${prefix}tagall3*
+╰────❍ *𝐵𝑅𝑈𝑋𝐼𝑁𝐻𝑂 𝑀𝑂𝐷𝑆 𝑊𝐴𝑅*`
+}
+
+exports.help = help
